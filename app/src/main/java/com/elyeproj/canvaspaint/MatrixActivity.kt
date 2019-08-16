14e56ddaf55dceec76e98739340c93438e62a48b
elye <elye.project@gmail.com>
2019-08-16 12:44:10 +1000

    Add more matrix color manipulation

--- app/src/main/java/com/elyeproj/canvaspaint/MatrixActivity.kt
+++ app/src/main/java/com/elyeproj/canvaspaint/MatrixActivity.kt
@@ -1,10 +1,12 @@
 package com.elyeproj.canvaspaint
 
+import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.CompoundButton
 import android.widget.RadioButton
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_matrix.*
 
 class MatrixActivity : AppCompatActivity() {
@@ -12,14 +14,90 @@ class MatrixActivity : AppCompatActivity() {
     private val radioList by lazy {
         listOf(
             radio_original, radio_red, radio_green, radio_blue, radio_gray,
-            radio_gray_red, radio_gray_green, radio_gray_blue, radio_inverse,
-            radio_binary, radio_black_white, radio_black_white_light, radio_gbr, radio_brg
+            radio_gray_red, radio_gray_green, radio_gray_blue, radio_inverse, radio_binary,
+            radio_black_white, radio_black_white_light, radio_gbr, radio_brg, radio_yuv
         )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matrix)
+
+        seek_saturation.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = ColorMatrix()
+                matrix.setSaturation((progress)/255f)
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_scale.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = ColorMatrix()
+                matrix.setScale((progress)/255f, (progress)/255f,(progress)/255f, 1f)
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_rotate_red.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = ColorMatrix()
+                matrix.setRotate(0, (progress).toFloat())
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_rotate_green.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = ColorMatrix()
+                matrix.setRotate(1, (progress).toFloat())
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_rotate_blue.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = ColorMatrix()
+                matrix.setRotate(2, (progress).toFloat())
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_black_white.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = floatArrayOf(
+                    85f, 85f, 85f, 0f, (progress - 255)*255f,
+                    85f, 85f, 85f, 0f, (progress - 255)*255f,
+                    85f, 85f, 85f, 0f, (progress - 255)*255f,
+                    0f, 0f, 0f, 1f, 0f)
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        seek_binary.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val matrix = floatArrayOf(
+                    255f, 0f, 0f, 0f, (progress - 255)*255f,
+                    0f, 255f, 0f, 0f, (progress - 255)*255f,
+                    0f, 0f, 255f, 0f, (progress - 255)*255f,
+                    0f, 0f, 0f, 1f, 0f)
+                image_background.colorFilter = ColorMatrixColorFilter(matrix)
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
 
         val listener: CompoundButton.OnCheckedChangeListener =
             CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
@@ -81,7 +159,7 @@ class MatrixActivity : AppCompatActivity() {
                             matrix = floatArrayOf(
                                 1f, 1f, 1f, 0f, 0f,
                                 0f, 0f, 0f, 0f, 0f,
-                                1f, 0f, 0f, 0f, 0f,
+                                0f, 0f, 0f, 0f, 0f,
                                 0f, 0f, 0f, 1f, 0f
                             )
                         )
@@ -166,10 +244,14 @@ class MatrixActivity : AppCompatActivity() {
                             )
                         )
                     }
+                    radio_yuv.id -> if (isChecked) {
+                        val matrix = ColorMatrix()
+                        matrix.setRGB2YUV()
+                        image_background.colorFilter = ColorMatrixColorFilter(matrix)
+                    }
                 }
                 uncheckedAllExcept(buttonView as RadioButton)
             }
-
 
         radioList.forEach {
             it.setOnCheckedChangeListener(listener)
